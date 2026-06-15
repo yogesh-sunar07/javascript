8ced578bf37827c39b0e9269bf5af24e136b6c39
yogesh-sunar07 <yogesu07@gmail.com>
2026-06-15 06:43:31 +0545

    variables

---
+++ 01_variables/index.js
@@ -0,0 +1,18 @@
+
+let name ="Yogesh"
+console.log(name);
+
+console.log(typeof name);
+
+// ways to declare variable in js 
+/*
+let (nodern & recommended)
+can be updated
+cannot be redecalred in same scope
+
+const (constant value)
+
+var(old method -avoid in modern js)
+can be redeclared
+has scope issuse-not recomended now 
+*/
