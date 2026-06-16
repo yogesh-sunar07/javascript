42ac56630a0e4f73fc08532251dd356455d1d541
yogesh-sunar07 <yogesu07@gmail.com>
2026-06-16 08:45:40 +0545

    datatype1

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -4,3 +4,21 @@
 
 console.log(3+3) ;//maintain code readability
 console.log("yogesh");
+
+
+let name = "yogesh"
+let age = 18 ;
+let isLoggedIn = false ;
+
+
+/*number => 2 to power 53
+  bigint'
+  string=>""
+  bollean => true/false
+  null => standalone value
+  undefined => undefined
+  symnol = to find the uniqueness
+  
+  
+  object 
+  null = object (typeof null)*/
