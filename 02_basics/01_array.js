cebbeab4ca1df5c9213544893037b0ab779f34c5
yogesh-sunar07 <yogesu07@gmail.com>
2026-06-19 18:08:11 +0545

    comt

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,43 +1,43 @@
-// // array
+// array
 
-// /*
-// Parentheses: ( )
-// Example: The meeting (which started late) ended quickly.
+/*
+Parentheses: ( )
+Example: The meeting (which started late) ended quickly.
 
-// Brackets (square brackets): [ ]
-// Example: She said, "[The report] needs revision."
+Brackets (square brackets): [ ]
+Example: She said, "[The report] needs revision."
 
-// Braces (curly brackets): { }
-// Example: Set A = {1, 2, 3, 4}.
-//  */
+Braces (curly brackets): { }
+Example: Set A = {1, 2, 3, 4}.
+ */
 
-// const myArr = [0,1,2,3,4,5]
-// const muHeros =["yogesh", "spiderman", "dipesh"]
+const myArr = [0,1,2,3,4,5]
+const muHeros =["yogesh", "spiderman", "dipesh"]
 
-// const myArr2 = new Array(1,2,3,4)
+const myArr2 = new Array(1,2,3,4)
 
-// console.log(myArr[7]);
-// console.log(myArr.length);
+console.log(myArr[7]);
+console.log(myArr.length);
 
 
-// // Array methods
+// Array methods
 
-// myArr.push(6)// insert form back in array 
-// myArr.push(7)
-// myArr.pop()// remove last array element
-// console.log(myArr);
+myArr.push(6)// insert form back in array 
+myArr.push(7)
+myArr.pop()// remove last array element
+console.log(myArr);
 
-// myArr.unshift(20)//insert from front side //if there is large data then its is time consiming
-// myArr.shift()// remove the element from front side 
-// console.log(myArr);
+myArr.unshift(20)//insert from front side //if there is large data then its is time consiming
+myArr.shift()// remove the element from front side 
+console.log(myArr);
 
-// const myArr3 = new Array(1,2,3,4,5,6)
-// console.log(myArr3.includes(9));// search there is 9 number in array 
-// console.log(myArr3.indexOf(3));
+const myArr3 = new Array(1,2,3,4,5,6)
+console.log(myArr3.includes(9));// search there is 9 number in array 
+console.log(myArr3.indexOf(3));
 
-// const newArr3 = myArr3.join()// join make the array (object)type  to string type
-// console.log(myArr3);
-// console.log(newArr3);
+const newArr3 = myArr3.join()// join make the array (object)type  to string type
+console.log(myArr3);
+console.log(newArr3);
 
 
 //slice,splice
