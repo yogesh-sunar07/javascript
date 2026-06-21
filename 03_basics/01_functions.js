bfef97b58e06f6c03011af0dda8b0f6f22604d0c
yogesh-sunar07 <yogesu07@gmail.com>
2026-06-21 22:12:47 +0545

    funparameter

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,22 +1,41 @@
-function myName(){
-    console.log("y");
-    console.log("o");
-    console.log("g");
-    console.log("e");
-    console.log("s");
-    console.log("h");
+// function myName(){
+//     console.log("y");
+//     console.log("o");
+//     console.log("g");
+//     console.log("e");
+//     console.log("s");
+//     console.log("h");
     
-}
-myName()
-// function addTwoNumber(a,b){//parameters
+// }
+// myName()
+// // function addTwoNumber(a,b){//parameters
+// //     console.log(a+b);
+    
+// // }
+// // addTwoNumber(3,9)//argument 
+// // console.log(addTwoNumber(3,5)); //undefine
+// function add2Number(a,b){
 //     console.log(a+b);
     
 // }
-// addTwoNumber(3,9)//argument 
-// console.log(addTwoNumber(3,5)); //undefine
-function add2Number(a,b){
-    console.log(a+b);
-    
+// const result = add2Number(3,9)
+// console.log("Result:", result); //undefine cuz function return nothing 
+
+// function mutiplyTwoN(a,b){
+ 
+//   return a*b
+// }
+
+// const  fesult = mutiplyTwoN(3,5)
+// console.log("result", fesult);
+
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("please enter a user name ");
+        return
+    }
+    return `${username} just logged in`
 }
-const result = add2Number(3,9)
-console.log("Result:", result); //undefine cuz function return nothing 
+console.log(loginUserMessage("yogesh"));
+ console.log(loginUserMessage());//undefined just login
