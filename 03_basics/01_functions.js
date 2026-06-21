890523bcac889b0eeae261cb47aadfd5f9899747
yogesh-sunar07 <yogesu07@gmail.com>
2026-06-21 16:56:09 +0545

    func

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+function myName(){
+    console.log("y");
+    console.log("o");
+    console.log("g");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+    
+}
+myName()
+// function addTwoNumber(a,b){//parameters
+//     console.log(a+b);
+    
+// }
+// addTwoNumber(3,9)//argument 
+// console.log(addTwoNumber(3,5)); //undefine
+function add2Number(a,b){
+    console.log(a+b);
+    
+}
+const result = add2Number(3,9)
+console.log("Result:", result); //undefine cuz function return nothing 
