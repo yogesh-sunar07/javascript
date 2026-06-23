cc764a8fd3c9f37fcc39e66271621546de29b867
yogesh-sunar07 <yogesu07@gmail.com>
2026-06-23 22:56:54 +0545

    scopelevel

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+// let a= 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "yogesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    //two()
+    console.log(username);
+    
+    
+}
+one()
