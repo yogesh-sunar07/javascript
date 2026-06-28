1511418dd13c9ea2e8404423f1fd8a893bd3e8be
yogesh-sunar07 <yogesu07@gmail.com>
2026-06-28 20:56:55 +0545

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    username: "yogesh",
+    price: 999,
+
+    welcomemessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        
+    }
+
+
+}
+
+// user.welcomemessage()
+// user.username = "dipesh"
+// user.welcomemessage()
+
+// console.log(this);// in node its give empty object in browser it give windows
+
+// function water(){
+//     let username = "yogesh"
+//     console.log(this.username);//undefine
+    
+// }
+// water()
+
+
+// const milk = function(){
+//     let username ="dipesh"
+//     console.log(this.username);
+    
+// }
+
+
+// const milk = ()=> {
+//     let username ="dipesh"
+//     console.log(this.username);
+    
+// }
+// milk()
+
+// const addtwo = (num1, num2)=>{
+//     return num1+num2
+// }
+
+const addthree = (num1,num2,num3)=> (num1+num2+num3)
+console.log(addthree(2,3,9))
+
+
